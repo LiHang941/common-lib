75821f2b7a151f03be9bead1e5e76cfe3797c318
lihang <lihang1329@gmail.com>
2018-12-14 15:06:02 +0800

    fix grpc bug

--- rest-samples/src/main/java/com/github/lihang941/example/resource/Test2Resource.java
+++ rest-samples/src/main/java/com/github/lihang941/example/resource/Test2Resource.java
@@ -10,14 +10,6 @@ import com.github.lihang941.example.entity.Test2;
 import com.github.lihang941.example.service.Test2Service;
 import com.github.lihang941.vertx.rest.*;
 import com.github.pagehelper.Page;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.vertx.core.http.HttpServerResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,13 +23,13 @@ import java.util.stream.Collectors;
  * @author : lihang1329@gmail.com
  * @since : 2018-12-03 10:02
  */
-@OpenAPIDefinition(info =
-@Info(
-        title = "the title",
-        version = "0.0",
-        description = "My API"
-)
-)
+//@OpenAPIDefinition(info =
+//@Info(
+//        title = "the title",
+//        version = "0.0",
+//        description = "My API"
+//)
+//)
 @URL("test2")
 @Controller
 public class Test2Resource {
@@ -48,12 +40,12 @@ public class Test2Resource {
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
 
-    @Operation(summary = "Get user by user name",
-            responses = {
-                    @ApiResponse(description = "The user",
-                            content = @Content(mediaType = "application/json",
-                                    schema = @Schema(implementation = Test2Dto.class))),
-                    @ApiResponse(responseCode = "400", description = "User not found")})
+//    @Operation(summary = "Get user by user name",
+//            responses = {
+//                    @ApiResponse(description = "The user",
+//                            content = @Content(mediaType = "application/json",
+//                                    schema = @Schema(implementation = Test2Dto.class))),
+//                    @ApiResponse(responseCode = "400", description = "User not found")})
     @POST
     public IdDto add(@Body Test2Dto test2Dto) {
         Test2 test2 = Test2Convert.toTest2().apply(test2Dto);
