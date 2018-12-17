97ecc0ac4ece86e17d30de24d9a0d57f7b103fad
lihang <lihang1329@gmail.com>
2018-12-17 19:22:04 +0800

    add tuple

---
+++ tuples/src/main/java/com/github/lihang941/tuple/EmptyTuple.java
@@ -0,0 +1,12 @@
+package com.github.lihang941.tuple;
+
+/**
+ * Empty Tuple type.
+ */
+public class EmptyTuple implements Tuple {
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
---
+++ tuples/src/main/java/com/github/lihang941/tuple/Tuple.java
@@ -0,0 +1,9 @@
+package com.github.lihang941.tuple;
+
+/**
+ * Tuple abstraction.
+ */
+public interface Tuple {
+
+    int getSize();
+}
